9a904f2985ddb73e2547fbd8bcdcb2b9df13c663
kaviarasu-thuraiarasu <kaviarasu.thuraiarasu@gmail.com>
2022-06-12 11:14:18 +0530

    Added New file changes

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from crypt import method
+
+app = Flask(__name__)
+
+@app.route('/',method=method.GET)
+def index():
+    return "application started"
+
+if __name__ =="__main__":
+ app.run(debug=True)
